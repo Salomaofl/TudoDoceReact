dbe20fc39c4244af1ac153968d8f6ec4aa3007e1
SalomaoFlima <100690807+Salomaofl@users.noreply.github.com>
2024-07-16 13:57:30 -0300

    Subindo as imagens do card

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './Services.css';
-import image1 from '../assets/color.jpg';
-import logo from '../assets/oi.jpg';
-import cupcake from '../assets/cupcake.jpg';
+import cardPP from '../assets/cardPP.png';
+import cardP from '../assets/cardP.png';
+import cardM from '../assets/cardM.png';
 
 
 
@@ -19,26 +19,27 @@ const Services = () => {
       <h2>Escolha o kit para sua festa</h2>
       <div className="card-container">
         <div className="card">
-          <img src={cupcake} alt="Serviço 1" />
+          <img src={cardPP} alt="Serviço 1" />
           <h3>Bolo PP</h3>
           <p>Incluso no pacote: 25 Doces tradicionais + Topo + Velinha.</p>
            <p>R$ 130,00.</p>
           <button onClick={() => handleChoose('Bolo PP', 'Incluso no pacote: 25 Doces tradicionais + Topo + Velinha.')}>Escolher</button>
         </div>
         <div className="card">
-          <img src={logo} alt="Serviço 2" />
+          <img src={cardP} alt="Serviço 2" />
           <h3>Bolo P</h3>
           <p>Incluso no pacote: 40 Doces tradicionais + Topo + Velinha</p>
           <p>R$ 185,00.</p>
           <button onClick={() => handleChoose('Bolo P', 'Incluso no pacote: 40 Doces tradicionais + Topo + Velinha')}>Escolher</button>
         </div>
         <div className="card">
-          <img src={image1} alt="Serviço 3" />
+          <img src={cardM} alt="Serviço 3" />
           <h3>Bolo M</h3>
           <p>Incluso no pacote: 60 Doces tradicionais + Topo + Velinha</p>
           <p>R$ 250,00.</p>
           <button onClick={() => handleChoose('Bolo M', 'Incluso no pacote: 60 Doces tradicionais + Topo + Velinha')}>Escolher</button>
         </div>
+
       </div>
     </section>
   );
